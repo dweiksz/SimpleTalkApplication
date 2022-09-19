bedb146265d7301e9be605ea9a2af7c831ce3c9f
Saleem Dweik <54520939+dweiksz@users.noreply.github.com>
2022-09-18 20:08:56 -0400

    Initial commit

---
+++ src/main/java/com/simpletalk/enterprise/SimpleTalkApplication.java
@@ -0,0 +1,13 @@
+package com.simpletalk.enterprise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleTalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleTalkApplication.class, args);
+	}
+
+}
