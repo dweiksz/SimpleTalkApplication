83eec04e9227676908b885d49d0c1b46aae2a767
handbrennan <handbd@mail.uc.edu>
2022-09-24 12:21:07 -0400

    Added Unit test to fetchById() using IPostStub.java and IPostService.Java

--- src/main/java/com/simpletalk/enterprise/service/IPostService.java
+++ src/main/java/com/simpletalk/enterprise/service/IPostService.java
@@ -3,6 +3,12 @@ package com.simpletalk.enterprise.service;
 import com.simpletalk.enterprise.dto.Post;
 
 public interface IPostService {
+    /**
+     * Fetch a postr with a given ID.
+     * @param id is a unique identifier for a post.
+     * @return the matching post, or null if no matches are found.
+     */
+    Post fetchById(int id);
 
     void save(Post post);
 }
--- src/test/java/com/simpletalk/enterprise/SimpleTalkApplicationTests.java
+++ src/test/java/com/simpletalk/enterprise/SimpleTalkApplicationTests.java
@@ -1,13 +1,40 @@
 package com.simpletalk.enterprise;
 
+import com.simpletalk.enterprise.dto.Post;
+import com.simpletalk.enterprise.service.IPostService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class SimpleTalkApplicationTests {
 
+    @Autowired
+    private IPostService postService;
+    private Post post;
+
     @Test
     void contextLoads() {
+    }
+    @Test
+    void fetchPostByID_returnsLOTRpostforID10(){
+        givenPostDataAreAvailable(); 
+        whenSearchPostWithID10();
+        thenReturnOneLOTRpostForID10();
+    }
+
+    private void givenPostDataAreAvailable() {
+    }
+
+    private void whenSearchPostWithID10() {
+        post = postService.fetchById(10);
+    }
+
+    private void thenReturnOneLOTRpostForID10() {
+        String message = post.getMessage();
+        assertEquals("I love Lord of the Rings!", message);
     }
 
 }
