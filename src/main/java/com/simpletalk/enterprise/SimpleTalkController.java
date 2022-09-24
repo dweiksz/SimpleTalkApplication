fc4d856e5a03d99b6bbbb2b3610f137bca04d68b
J-Macleod <jacob.macleodnin@gmail.com>
2022-09-24 00:59:06 -0400

    Basic UI and Thymeleaf Implementation

--- src/main/java/com/simpletalk/enterprise/SimpleTalkController.java
+++ src/main/java/com/simpletalk/enterprise/SimpleTalkController.java
@@ -1,6 +1,8 @@
 package com.simpletalk.enterprise;
 
+import com.simpletalk.enterprise.dto.Post;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -8,7 +10,14 @@ public class SimpleTalkController {
 
     // Handles root or (/) endpoint and returns start page
     @RequestMapping("/")
-    public String index() {
+    public String index(Model model) {
+        Post post = new Post();
+        post.setPostID(0);
+        post.setUserID("0");
+        post.setUserName("TestUser0");
+        post.setMessage("This is a test post.");
+        post.setLikes(2);
+        model.addAttribute(post);
         return "startPage";
     }
 
