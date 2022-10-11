f4ed5e7e4c2c938d6d7fd874211f3158732e6b59
handbrennan <handbd@mail.uc.edu>
2022-10-10 22:09:54 -0400

    fixed missing commit from merge (all tests pass, No errors)

--- src/test/java/com/simpletalk/enterprise/SimpleTalkApplicationTests.java
+++ src/test/java/com/simpletalk/enterprise/SimpleTalkApplicationTests.java
@@ -1,26 +1,35 @@
 package com.simpletalk.enterprise;
 
+import com.simpletalk.enterprise.dao.IPostDAO;
 import com.simpletalk.enterprise.dto.Post;
 import com.simpletalk.enterprise.service.IPostService;
+import com.simpletalk.enterprise.service.PostServiceStub;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.verify;
 
 @SpringBootTest
 class SimpleTalkApplicationTests {
 
     @Autowired
     private IPostService postService;
-    private Post post;
+    private Post post = new Post();
+
+
+    @MockBean
+    private IPostDAO postDAO;
 
     @Test
     void contextLoads() {
     }
     @Test
-    void fetchPostByID_returnsLOTRPostForID10(){
+    void fetchPostByID_returnsLOTRPostForID10() throws Exception {
         givenPostDataAreAvailable();
         whenPost10AddedIsILoveLOTR();
         whenSearchPostWithID10();
@@ -28,14 +37,20 @@ class SimpleTalkApplicationTests {
     }
 
 
-    private void givenPostDataAreAvailable() {
+    private void givenPostDataAreAvailable() throws Exception{
+        Mockito.when(postDAO.save(post)).thenReturn(post);
+        postService = new PostServiceStub(postDAO);
     }
 
     private void whenPost10AddedIsILoveLOTR() {
         Post LOTR = new Post();
         LOTR.setPostID(10);
         LOTR.setMessage("I love Lord of the Rings!");
-        Mockito.when(postDAO.fetch(10)).thenReturn(postDAO)
+        try {
+            Mockito.when(postDAO.fetch(10)).thenReturn((LOTR));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private void whenSearchPostWithID10() {
@@ -51,4 +66,20 @@ class SimpleTalkApplicationTests {
         assertEquals("I love Lord of the Rings!", message);
     }
 
+    @Test
+    void savePost_validateReturnPostWithUserID() throws Exception  {
+        givenPostDataAreAvailable();
+        whenUserCreatesANewPostAndSaves();
+        thenCrateNewPostRecordAndReturnIt();
+    }
+
+    private void whenUserCreatesANewPostAndSaves() {
+
+        post.setUserID(100);
+    }
+
+    private void thenCrateNewPostRecordAndReturnIt() throws Exception  {
+        Post createPost = postService.save(post);
+        assertEquals(post, createPost);
+    }
 }
