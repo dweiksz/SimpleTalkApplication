45a408ff09ac25e62e97bd5d3a6eabe13bef0bd1
Saleem Dweik <54520939+dweiksz@users.noreply.github.com>
2022-11-02 19:47:44 -0400

    Update IPostService.java

--- src/main/java/com/simpletalk/enterprise/service/IPostService.java
+++ src/main/java/com/simpletalk/enterprise/service/IPostService.java
@@ -1,12 +1,10 @@
 package com.simpletalk.enterprise.service;
-
-import com.simpletalk.enterprise.dto.Post;
 import com.simpletalk.enterprise.dto.Thread;
 import com.simpletalk.enterprise.dto.User;
-
+import com.simpletalk.enterprise.dto.Post;
 import java.io.IOException;
-import java.util.List;
 
+import java.util.List;
 public interface IPostService {
     /**
      * Fetch a post with a given ID.
@@ -14,6 +12,7 @@ public interface IPostService {
      * @return the matching post, or null if no matches are found.
      */
     Post fetchById(int id) ;
+
     /**
      * Save a new Journal Entry
      *
@@ -26,7 +25,30 @@ public interface IPostService {
      * Return all posts.
      * @return a collection of all posts.
      */
-
     List<Post> fetchAll() throws Exception;
+
     void delete(int id) throws  Exception;
+
+    /**
+     * Fetch a post with a given ID.
+     * @param id is a unique identifier for a thread.
+     * @return the matching thread, or null if no matches are found.
+     */
+    Thread threadFetchById(int id) ;
+
+    /**
+     * Save a new Thread
+     *
+     * @param thread the entry to save.
+     * @return
+     */
+    Thread threadSave(Thread thread) throws Exception;
+
+    /**
+     * Return all threads.
+     * @return a collection of all posts.
+     */
+    List<Thread> threadFetchAll() throws Exception;
+
+    void threadDelete(int id) throws  Exception;
 }
