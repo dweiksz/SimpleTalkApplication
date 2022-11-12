15c81b2d6f49af15e3dc9c5c351866df81f5654b
handbrennan <handbd@mail.uc.edu>
2022-11-12 16:50:51 -0500

    Updated Auth Contoller

--- src/main/java/com/simpletalk/enterprise/controller/AuthController.java
+++ src/main/java/com/simpletalk/enterprise/controller/AuthController.java
@@ -1,10 +1,16 @@
 package com.simpletalk.enterprise.controller;
 
+import com.simpletalk.enterprise.dto.AuthenticationResponse;
+import com.simpletalk.enterprise.dto.LoginRequest;
+import com.simpletalk.enterprise.dto.RefreshTokenRequest;
 import com.simpletalk.enterprise.dto.RegisterRequest;
 import com.simpletalk.enterprise.service.AuthService;
+import com.simpletalk.enterprise.service.RefreshTokenService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 import static org.springframework.http.HttpStatus.OK;
 
@@ -14,6 +20,7 @@ import static org.springframework.http.HttpStatus.OK;
 public class AuthController {
 
     private final AuthService authService;
+    private final RefreshTokenService refreshTokenService;
 
     @PostMapping("/signup")
     public ResponseEntity<String> signup(@RequestBody RegisterRequest registerRequest) {
@@ -25,6 +32,20 @@ public class AuthController {
     public ResponseEntity<String> verifyAccount(@PathVariable String token) {
         authService.verifyAccount(token);
         return new ResponseEntity<>("Account Activated Successfully", OK);
+    }
+    @PostMapping("/login")
+    public AuthenticationResponse login(@RequestBody LoginRequest loginRequest) {
+        return authService.login(loginRequest);
+    }
+    @PostMapping("/refresh/token")
+    public AuthenticationResponse refreshTokens(@Valid @RequestBody RefreshTokenRequest refreshTokenRequest) {
+        return authService.refreshToken(refreshTokenRequest);
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<String> logout(@Valid @RequestBody RefreshTokenRequest refreshTokenRequest) {
+        refreshTokenService.deleteRefreshToken(refreshTokenRequest.getRefreshToken());
+        return ResponseEntity.status(OK).body("Refresh Token Deleted Successfully!!");
     }
 
 }
