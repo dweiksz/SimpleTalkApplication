0847d73cf469854f748f4d1770572b489fcc9b19
Anish Selar <anish.selar@gmail.com>
2022-11-13 19:19:14 -0500

    created Settings page

--- src/main/java/com/simpletalk/enterprise/SimpleTalkApplication.java
+++ src/main/java/com/simpletalk/enterprise/SimpleTalkApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SimpleTalkApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(SimpleTalkApplication.class, args);
     }
 
