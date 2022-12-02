896d533f4c29cab5239fd4cdddb82427cc9a324c
handbrennan <handbd@mail.uc.edu>
2022-12-01 19:11:07 -0500

    Updated Postmappeer fields and deleted isLoggedIn method in Auth Service

--- src/main/java/com/simpletalk/enterprise/mapper/PostMapper.java
+++ src/main/java/com/simpletalk/enterprise/mapper/PostMapper.java
@@ -1,30 +1,29 @@
 package com.simpletalk.enterprise.mapper;
 
-
 import com.github.marlonlom.utilities.timeago.TimeAgo;
 import com.simpletalk.enterprise.dto.PostRequest;
 import com.simpletalk.enterprise.dto.PostResponse;
 import com.simpletalk.enterprise.model.Post;
 import com.simpletalk.enterprise.model.Thread;
 import com.simpletalk.enterprise.model.User;
-import com.simpletalk.enterprise.service.AuthService;
+import com.simpletalk.enterprise.model.*;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.springframework.beans.factory.annotation.Autowired;
 
-@Mapper(componentModel = "spring")
+
+@Mapper (componentModel = "spring")
 public abstract class PostMapper {
 
-    @Autowired
-    private AuthService authService;
-
-
+    @Mapping(target = "createdDate", expression = "java(java.time.Instant.now())")
     @Mapping(target = "description", source = "postRequest.description")
     @Mapping(target = "thread", source = "thread")
     @Mapping(target = "user", source = "user")
     public abstract Post map(PostRequest postRequest, Thread thread, User user);
 
     @Mapping(target = "id", source = "postId")
+    @Mapping(target = "postName", source = "postName")
+    @Mapping(target = "description", source = "description")
+    @Mapping(target = "url", source = "url")
     @Mapping(target = "threadName", source = "thread.name")
     @Mapping(target = "userName", source = "user.username")
     @Mapping(target = "duration", expression = "java(getDuration(post))")
@@ -33,5 +32,4 @@ public abstract class PostMapper {
     String getDuration(Post post) {
         return TimeAgo.using(post.getCreatedDate().toEpochMilli());
     }
-
 }
--- src/main/java/com/simpletalk/enterprise/service/AuthService.java
+++ src/main/java/com/simpletalk/enterprise/service/AuthService.java
@@ -109,8 +109,4 @@ public class AuthService {
                 .build();
     }
 
-    public boolean isLoggedIn() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        return !(authentication instanceof AnonymousAuthenticationToken) && authentication.isAuthenticated();
-    }
 }
